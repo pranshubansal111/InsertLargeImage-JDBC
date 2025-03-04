204716ca89aff013118d456d0d11164c7d4c22f0
pranshubansal111 <pranshubansal111@gmail.com>
2025-03-05 02:53:20 +0530

    added necessary comments

--- src/main/java/org/jdbcproject/Main.java
+++ src/main/java/org/jdbcproject/Main.java
@@ -12,24 +12,32 @@ public class Main {
     
     public static void main(String[] args) throws SQLException, FileNotFoundException {
 
+        //establishing connection
         Connection con = ConnectionProvider.getConnection();
 
+        //query to create table
         String createTableQuery = "create table if not exists student (" +
                 "st_id int primary key auto_increment," +
                 "st_img longblob" +
                 ");";
 
+        //creating statement
         Statement st = con.createStatement();
 
+        //executing statement
         st.executeUpdate(createTableQuery);
 
+        //query to insert image to db
         String insertImageQuery = "insert into student(st_img) values (?);";
+
         File file = new File("C:\\Users\\Pranshu Bansal\\Downloads\\img1.jpg");
         FileInputStream fis = new FileInputStream(file);
 
+        //preparing statement
         PreparedStatement ps = con.prepareStatement(insertImageQuery);
         ps.setBinaryStream(1, fis, (int)file.length());
-        ps.executeUpdate();
 
+        //executing statement
+        ps.executeUpdate();
     }
 }
